6d3495e1f1a0e7b1b0d99d24583cfa0f582e22b6
mbatilando <mari.batilando@gmail.com>
2013-04-28 00:20:54 -0700

    init commit

---
+++ assets/js/custom.js
@@ -0,0 +1,32 @@
+ $(document).ready(function() {
+
+ 	$("#homeBut").click(function(){
+	  $('#homeSec').ScrollTo({
+	    duration: 2000,
+	    easing: 'linear'
+		});
+	});
+
+	$("#projBut").click(function(){
+	  $('#projSec').ScrollTo({
+	    duration: 2000,
+	    easing: 'linear'
+		});
+	});
+
+	$("#teamBut").click(function(){
+	  $('#teamSec').ScrollTo({
+	    duration: 2000,
+	    easing: 'linear'
+		});
+	});
+
+
+
+	$("#contactBut").click(function(){
+	  $('#contactSec').ScrollTo({
+	    duration: 2000,
+	    easing: 'linear'
+		});
+	});
+});
