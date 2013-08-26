0bf739d7ccb1a5d3c73e3a4077fad08390ee337f
Michael Lannin <mlannin23@gmail.com>
2013-08-26 13:10:46 -0700

    Updates for Fall 2013
    Removed Contact Us section because the form didn't work, added Events
    section for facebook event, changed projects section to old format so
    descriptions are present

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,12 @@
  $(document).ready(function() {
 
+ 	$("#eventsBut").click(function(){
+	  $('#eventsSec').ScrollTo({
+	    duration: 200,
+	    easing: 'linear'
+		});
+	});
+
  	$("#aboutBut").click(function(){
 	  $('#aboutSec').ScrollTo({
 	    duration: 200,
